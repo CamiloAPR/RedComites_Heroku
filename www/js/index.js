2ff8865983e8632a9506d32e8fae4927dd3ad2e3
CamiloAPR <palaciocamilor@gmail.com>
2017-06-09 04:33:44 -0500

    fix

--- www/js/index.js
+++ www/js/index.js
@@ -104,11 +104,11 @@ $(document).on("pagebeforeshow","#committee",function(){
         // '<div class="public-imagen"></div>'+
       '</div></div>';
 
-  }
+
   var dataPublicationsTemplate = [{field: "id"},{field: "title"},{field: "color"}, {field: "content"},{field: "publication_date"}]
   ux_service.createHTMLComponents(publicationsTemplate, dataPublicationsTemplate, $("#committeePublications"), data["publications"]);
   console.log(data["publications"]);
-
+  }
   if(urlParam("committee") !== undefined){
     db_service.get("committee/committee_id/"+urlParam("committee"), callback);
   }
